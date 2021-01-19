afc3b378057d361e5a6f217013e83ccb7f7471c8
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-01-19 12:16:54 +0700

    menambahkan getbyname

--- SampleWebForm/DAL/EmployeeDAL.cs
+++ SampleWebForm/DAL/EmployeeDAL.cs
@@ -25,5 +25,41 @@ namespace SampleWebForm.DAL
                 return results;
             }
         }
+
+        public Employee GetById(int empId)
+        {
+            using(SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Employees where EmpId=@EmpId";
+                var param = new { EmpId = empId };
+                var result = conn.QuerySingle<Employee>(strSql,param);
+                return result;
+            }
+        }
+
+        public IEnumerable<Employee> GetSearch(string keyword)
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Employees where EmpName like '%'+@keyword+'%' 
+                OR Department like '%'+@keyword+'%' OR Designation like '%'+@keyword+'%' OR 
+                Qualification like '%'+@keyword+'%' order by EmpName asc";
+                var param = new { keyword = keyword };
+                var results = conn.Query<Employee>(strSql, param);
+                return results;
+            }
+        }
+
+        public IEnumerable<Employee> GetByNameAndDept(string empName,string dept)
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Employees where EmpName like '%'+@EmpName+'%' 
+                AND Department like '%'+@Department+'%' order by EmpName asc";
+                var param = new { EmpName = empName, Department = dept };
+                var results = conn.Query<Employee>(strSql, param);
+                return results;
+            }
+        }
     }
 }
