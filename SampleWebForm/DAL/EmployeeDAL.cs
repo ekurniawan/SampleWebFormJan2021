a6b536bf352e038f087ad1e7741d642bfb8f13b9
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-01-19 11:51:40 +0700

    menambahkan dapper

---
+++ SampleWebForm/DAL/EmployeeDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using Dapper;
+using SampleWebForm.Models;
+
+namespace SampleWebForm.DAL
+{
+    public class EmployeeDAL
+    {
+        private string GetConnStr()
+        {
+            return WebConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Employees order by EmpName asc";
+                var results = conn.Query<Employee>(strSql);
+                return results;
+            }
+        }
+    }
+}
