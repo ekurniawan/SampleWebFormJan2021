2e9f4b23c5379f6ccb494aa90a88482e0405d08a
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-01-25 11:30:55 +0700

    menambahkan class pengguna

---
+++ SampleWebForm/DAL/RegistrasiDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace SampleWebForm.DAL
+{
+    public class RegistrasiDAL
+    {
+        private string GetConnStr()
+        {
+            return WebConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select CategoryID,CategoryName,Description from Categories order by CategoryName asc";
+                var results = conn.Query<Category>(strSql);
+                return results;
+            }
+        }
+    }
+}
