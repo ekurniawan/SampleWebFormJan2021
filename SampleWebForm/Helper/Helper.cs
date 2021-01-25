4be606f00fde8c9e2fc4659f7f64125d85bee7a8
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-01-25 11:10:34 +0700

    menambahkan md5

---
+++ SampleWebForm/Helper/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SampleWebForm.Helper
+{
+    public class Helper
+    {
+        public static string GetMd5Hash(string password)
+        {
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(password);
+            byte[] hash = 
+                ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+            string encoded = BitConverter.ToString(hash).Replace("-",string.Empty).ToLower();
+            return encoded;
+        }
+    }
+}
